6712fbe14a294123a7893d2a76b1ba9f62d86c60
Yahya Meksen <yahya.exe07@gmail.com>
2026-09-08 18:29:14 +0100

    feat: initialize products, redis, and prisma modules with dependency injection configuration

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService , PrismaService],
+  providers: [ProductsService , PrismaService , ConfigService],
 })
 export class ProductsModule {}
