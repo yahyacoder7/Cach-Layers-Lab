972c8a89f7084557412bd201c3863f3b607122fe
Yahya Meksen <yahya.exe07@gmail.com>
2026-09-08 23:16:15 +0100

    refactor(config):
    make ConfigModule global, drop redundant ConfigService providers

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,10 +1,9 @@
 import { Global, Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
-import { ConfigService } from '@nestjs/config';
 
 @Global()
 @Module({
-  providers: [RedisService , ConfigService],
+  providers: [RedisService],
   exports: [RedisService],
 })
 export class RedisModule {}
