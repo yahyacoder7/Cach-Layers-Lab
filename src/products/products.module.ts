1a2742b7e50f34ac56f9961032e344a6fb2b361a
Yahya Meksen <yahya.exe07@gmail.com>
2026-09-12 01:13:41 +0100

    refactor(products): remove duplicate SupabaseService

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { SupabaseService } from '../supabase/supabase.service';
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService , PrismaService],
+  providers: [ProductsService , PrismaService , SupabaseService],
 })
 export class ProductsModule {}
