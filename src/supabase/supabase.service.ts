40f5b7ba9765649864a0a08d4c7354b110606616
Yahya Meksen <yahya.exe07@gmail.com>
2026-09-12 01:12:46 +0100

    feat(supabase): image upload, public URL, bucket check

--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -4,45 +4,49 @@ import { ConfigService } from '@nestjs/config'; // reads .env
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import * as HttpErrors from '../common/http-errors';
 
-
-interface UploadOptions {
-  fieldname: string,
-  originalname: string,
-  encoding: string,
-  mimetype: string,
-  buffer: Buffer,
-  size: number,
-}
-
-
 @Injectable()
 export class SupabaseService implements OnModuleInit {
-  private readonly logger = new Logger("Supabase");
+  private readonly logger = new Logger('Supabase');
   private readonly client: SupabaseClient;
-  private readonly config = new ConfigService();
 
-  constructor() {
+  constructor(private readonly config: ConfigService) {
     this.client = createClient(
       this.config.getOrThrow<string>('SUPABASE_URL'),
       this.config.getOrThrow<string>('SUPABASE_SERVICE_ROLE_KEY'),
     );
   }
 
- async uploadImage(uploadOptions: UploadOptions){
-    const {fieldname , originalname , encoding , mimetype , buffer , size} = uploadOptions;
-     
- }
+  async uploadImage(file: Express.Multer.File): Promise<string> {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    const filePath = `product/${fileName}`;
+    const fileBuffer = file.buffer;
+    const fileType = file.mimetype;
+
+    const { data, error } = await this.client.storage
+      .from('Cach-Lab-Images')
+      .upload(filePath, fileBuffer, {
+        contentType: fileType,
+        upsert: false,
+      });
 
 
-  
+      if (error){
+        throw new HttpErrors.InternalServerErrorException(`Supabase Upload Error: ${error.message}`);
+      }
+      return filePath;
+  }
+
+  async getImageUrl(filePath: string): Promise<string> {
+    const { data: { publicUrl } } = this.client.storage.from('Cach-Lab-Images').getPublicUrl(filePath);
+    return publicUrl;
+  }
 
   async onModuleInit() {
-    try {
+   
       const { data, error } = await this.client.storage.listBuckets();
 
-      this.logger.log("Connection successing to supabase")
-    } catch (e) {
-      this.logger.error('Error connecting to supabase', e); 
-    }
+      this.logger.log('Connection successing to supabase');
+  if(error) this.logger.error('Error connecting to supabase', error);
+  
   }
 }
